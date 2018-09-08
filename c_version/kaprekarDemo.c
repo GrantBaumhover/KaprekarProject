166942bc9d878c7e5160531249328f5a5db68234
Grant Baumhover <42647879+GrantBaumhover@users.noreply.github.com>
2018-09-07 22:01:36 -0500

    Update kaprekarDemo.c
    print f must be below main

--- c_version/kaprekarDemo.c
+++ c_version/kaprekarDemo.c
@@ -2,10 +2,10 @@
 #include<stdio.h>
 #include<math.h>
 
- printf("kaprekarUtils.h\n");
-
 int main(int argc, char **argv) {
-
+ 
+printf("kaprekarUtils.h\n");
+ 
   if(argc != 2) {
     printf(stderr, "Usage: %s n\n", argv[0]);
     exit(1);
