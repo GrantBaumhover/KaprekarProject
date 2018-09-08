5b4298515d8f8147a5d2a73578346d83bb441401
Grant Baumhover <42647879+GrantBaumhover@users.noreply.github.com>
2018-09-07 22:02:02 -0500

    Update kaprekarDemo.c
    Not needed

--- c_version/kaprekarDemo.c
+++ c_version/kaprekarDemo.c
@@ -7,7 +7,7 @@ int main(int argc, char **argv) {
 printf("kaprekarUtils.h\n");
  
   if(argc != 2) {
-    printf(stderr, "Usage: %s n\n", argv[0]);
+    printf("Usage: %s n\n", argv[0]);
     exit(1);
   }
 
